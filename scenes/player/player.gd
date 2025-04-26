2c246df199b87a535110b7bf1fec55a84b0917b6
Piggward <Axel.stromberg@hotmail.com>
2025-04-26 18:42:55 +0200

    hand_cursor

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -2,12 +2,23 @@ class_name Player
 extends CharacterBody2D
 
 var holding: ThrowableBody = null
+@onready var cursor = $AnimatedSprite2D
+const HAND_ICONS_1 = preload("res://scenes/player/HandIcons1.png")
+const HAND_ICONS_2 = preload("res://scenes/player/HandIcons2.png")
+const HAND_CLOSED = preload("res://scenes/player/hand_closed.png")
+const HAND_OPEN = preload("res://scenes/player/hand_open.png")
+
+func _ready():
+	Input.set_custom_mouse_cursor(HAND_OPEN)
+	pass
 
 func can_pick_up():
 	return holding == null
 	
 func release():
 	holding = null
+	Input.set_custom_mouse_cursor(HAND_OPEN)
 	
 func pick_up(body: ThrowableBody): 
 	holding = body
+	Input.set_custom_mouse_cursor(HAND_CLOSED)
