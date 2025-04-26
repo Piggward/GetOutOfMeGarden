5df4bf7aedece09775d1037c9bbaf507a987c0e4
Markuss Sprogis <markuss.sprogis@dataductus.se>
2025-04-26 23:35:59 +0200

    Add fisherman and fish

---
+++ scenes/fish/fisherman.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+@onready var fish_anim = $FishAnim
+@onready var spawn_point = $SpawnPoint
+var fish_scene = preload("res://scenes/fish/fish.tscn")
+@export var spawn_paths: Array[Path2D] = []
+
+func spawn_fish():
+	fish_anim.play("hook")
+	var fish = fish_scene.instantiate()
+	var random_path = randi_range(0, spawn_paths.size()-1)
+	print(random_path)
+	fish.spawn(spawn_paths[random_path].get_child(0))
+	spawn_paths[random_path].get_child(0).add_child(fish)
+	fish.position = Vector2.ZERO
+
+
+func _on_fish_anim_animation_finished() -> void:
+	if fish_anim.animation == "hook":
+		fish_anim.play("throw")
+	elif fish_anim.animation == "throw":
+		fish_anim.play("default")
+	
