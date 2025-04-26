75b4acbfdcc3430709ecff57c575f5c24bc8c431
Jakob Andersson <jakob@plick.se>
2025-04-26 13:59:59 +0200

    fix spawned object z-index

--- scripts/spawn_manager.gd
+++ scripts/spawn_manager.gd
@@ -10,17 +10,14 @@ var spawnables: Dictionary = {
 	"flower": {
 		"scene": preload("res://scenes/flower.tscn"),
 		"size": Vector2(16,16)
+	},
+	"weed": {
+		"scene": preload("res://scenes/weeds_art.tscn"),
+		"size": Vector2(16,16)
 	}
 }
 
 func _ready():
-	# Set the spawn area size according to the input values
-	spawn_area.position = spawn_area.position  # Just to ensure it's correctly placed in the scene
-	spawn_area.scale = Vector2.ONE  # Reset scale, we won't use it anymore
-	spawn_area.set("rect_size", Vector2(spawn_area_width, spawn_area_height))  # Adjust size
-
-	queue_redraw()  # Draw the debug border
-	
 	debug_spawn_loop()
 
 func spawn_object(object_name: String) -> void:
@@ -36,7 +33,6 @@ func spawn_object(object_name: String) -> void:
 		if is_space_free_at_position(pos, object_size):
 			var instance = object_scene.instantiate()
 			instance.position = pos
-			instance.z_index = 10  # Make sure it renders in front
 			spawned_objects.add_child(instance)
 			return
 	print("No available space found to spawn object: [%s] with size: [%s]." % [object_name, object_size])
@@ -62,16 +58,10 @@ func is_space_free_at_position(pos: Vector2, size: Vector2) -> bool:
 	params.collide_with_areas = true
 
 	return space_state.intersect_shape(params, 1).is_empty()
-
-func _draw() -> void: # TODO: remove me to get rid of the green debug-border of spawnarea
-	# Draw the spawn area size using the input dimensions
-	var size = Vector2(spawn_area_width, spawn_area_height)
-	var half = size / 2
-	draw_rect(Rect2(spawn_area.position - half, size), Color.GREEN, false, 2.0)
 	
 func debug_spawn_loop() -> void:
 	while true:
-		spawn_object("flower")
-		await get_tree().create_timer(1.0).timeout
+		spawn_object("weed")
+		await get_tree().create_timer(.1).timeout
 		
 	
