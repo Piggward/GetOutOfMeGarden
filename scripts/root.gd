9d7de9428dc79f2d0d9f7617612f3674f996c048
Markuss Sprogis <markuss.sprogis@dataductus.se>
2025-04-27 09:35:41 +0200

    add audio
    gräs växer
    rot växer
    gräs klippt
    plocka upp tool

--- scripts/root.gd
+++ scripts/root.gd
@@ -3,6 +3,16 @@ extends InteractableArea
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $root_art/AnimatedSprite2D
 const GREEN_EXPLOSION_PARTICLES = preload("res://scenes/green_explosion_particles.tscn")
+var spawn_sounds = [
+	preload("res://assets/audio/root_spawn_1.wav"),
+	preload("res://assets/audio/root_spawn_2.wav"),
+]
+
+func _ready() -> void:
+	var audio = $AudioStreamPlayer2D
+	audio.stream = spawn_sounds[randi_range(0, spawn_sounds.size()-1)]
+	audio.pitch_scale = 1.0 + randf_range(-0.3, -0.2)
+	audio.play()
 
 func interact(tool_used: String = ""):
 	super.interact()
--- scripts/weed.gd
+++ scripts/weed.gd
@@ -4,6 +4,26 @@ extends InteractableArea
 @onready var animated_sprite_2d: AnimatedSprite2D = $weed_art/AnimatedSprite2D
 const GREEN_EXPLOSION_PARTICLES = preload("res://scenes/green_explosion_particles.tscn")
 
+var spawn_sounds = [
+	preload("res://assets/audio/grass_spawn.wav"),
+	preload("res://assets/audio/grass_spawn_2.wav"),
+	preload("res://assets/audio/grass_spawn_3.wav"),
+	preload("res://assets/audio/grass_spawn_4.wav"),
+	preload("res://assets/audio/grass_spawn_5.wav"),
+	preload("res://assets/audio/grass_spawn_6.wav"),
+	preload("res://assets/audio/grass_spawn_7.wav"),
+	preload("res://assets/audio/grass_spawn_8.wav")
+]
+
+var death_sounds = [
+	preload("res://assets/audio/cut_grass.wav"),
+]
+
+func _ready() -> void:
+	var audio = $AudioStreamPlayer2D
+	audio.stream = spawn_sounds[randi_range(0, spawn_sounds.size()-1)]
+	audio.pitch_scale = 1.0 + randf_range(0.2, 0.4)
+	audio.play()
 func interact(tool_used: String = ""):
 	super.interact()
 	if tool_used == "scissors":
@@ -19,6 +39,12 @@ func damage():
 	else:
 		animated_sprite_2d.frame -= 1
 	
+	var audio = $AudioStreamPlayer2D
+	audio.stream = death_sounds[randi_range(0, death_sounds.size()-1)]
+	audio.pitch_scale = randf_range(1.0, 1.6)
+	audio.play()
+
 func kill():
+	animated_sprite_2d.visible = false
 	die.emit(self)
 	self.queue_free()
