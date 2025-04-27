5b943ff168f0f88509a81b546bdc1460871de6b7
Markuss Sprogis <markuss.sprogis@dataductus.se>
2025-04-27 17:53:46 +0200

    try 7 levels

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -8,13 +8,27 @@ var root_timer:Timer
 @export var wave2_weed_amount:float
 @export var wave3_weed_amount:float
 @export var wave4_weed_amount:float
+@export var wave5_weed_amount:float
+@export var wave6_weed_amount:float
+@export var wave7_weed_amount:float
+
 @export var wave2_root_amount:float
 @export var wave3_root_amount:float
 @export var wave4_root_amount:float
+@export var wave5_root_amount:float
+@export var wave6_root_amount:float
+@export var wave7_root_amount:float
+
 @export var wave3_bunny_amount:float
 @export var wave4_bunny_amount:float
+@export var wave5_bunny_amount:float
+@export var wave6_bunny_amount:float
+@export var wave7_bunny_amount:float
 
-@export var wave4_fish_timer:float = 10
+@export var wave4_fish_timer:float = 12
+@export var wave5_fish_timer:float = 12
+@export var wave6_fish_timer:float = 12
+@export var wave7_fish_timer:float = 12
 
 
 enum GameState {
@@ -22,8 +36,13 @@ enum GameState {
 	WAVE_1,
 	WAVE_2,
 	WAVE_3,
-	WAVE_4
+	WAVE_4,
+	WAVE_5,
+	WAVE_6,
+	WAVE_7
 }
+
+var wave_length = [	13,	13,	13, 13, 13, 45,	45,	45]
 
 @onready var bunny_manager: Node2D = $"../BunnyManager"
 @onready var fisherman: Node2D = $"../Fisherman"
@@ -60,11 +79,15 @@ func next_wave() -> GameState:
 		GameState.WAVE_1: return GameState.WAVE_2
 		GameState.WAVE_2: return GameState.WAVE_3
 		GameState.WAVE_3: return GameState.WAVE_4
+		GameState.WAVE_4: return GameState.WAVE_5
+		GameState.WAVE_5: return GameState.WAVE_6
+		GameState.WAVE_6: return GameState.WAVE_7
 	print("[GameManager#next_wave] Unkown current_state: [%s], unable to continue... returning to GameStateSTART" % current_state)
 	return GameState.START
 	
 func _on_start_next_wave_timer_timeout() -> void:
-	if current_state == GameState.WAVE_4: 
+	if current_state == GameState.WAVE_7:
+		flower_bed.set_growth_stage(GameState.WAVE_4)
 		Global.game_over.emit(true)
 	var next_state = next_wave()
 	print("[GameManager#_on_start_next_wave_timer_timeout] Update state from: [%s], to: [%s]" % [current_state, next_state])
@@ -81,13 +104,34 @@ func _on_start_next_wave_timer_timeout() -> void:
 		GameState.WAVE_3:
 			bunny_manager._set_spawn_rate(wave3_bunny_amount)
 			bunny_manager.set_spawn(true)
+			#bunny_manager.set_spawn(true)
 			weed_timer.wait_time=wave3_weed_amount
-			root_timer.wait_time = wave3_root_amount
+			root_timer.wait_time = wave3_root_amount			
+			flower_progress = GameState.WAVE_2
 		GameState.WAVE_4:
-			weed_timer.wait_time=wave4_weed_amount
-			root_timer.wait_time = wave4_root_amount
 			bunny_manager._set_spawn_rate(wave4_bunny_amount)
-			fisherman.start_fish_spawn_timer(wave4_fish_timer)
+			weed_timer.wait_time=wave4_weed_amount
+			root_timer.wait_time = wave4_root_amount			
+			flower_progress = GameState.WAVE_2
+		GameState.WAVE_5:
+			bunny_manager._set_spawn_rate(wave5_bunny_amount)
+			bunny_manager.set_spawn(true)
+			weed_timer.wait_time=wave5_weed_amount
+			root_timer.wait_time = wave5_root_amount			
+			flower_progress = GameState.WAVE_2
+			fisherman.start_fish_spawn_timer(wave5_fish_timer)
+		GameState.WAVE_6:
+			bunny_manager._set_spawn_rate(wave6_bunny_amount)
+			#bunny_manager.set_spawn(true)
+			weed_timer.wait_time=wave6_weed_amount
+			root_timer.wait_time = wave6_root_amount			
+			flower_progress = GameState.WAVE_3
+			fisherman.start_fish_spawn_timer(wave6_fish_timer)
+		GameState.WAVE_7:
+			weed_timer.wait_time=wave7_weed_amount
+			root_timer.wait_time = wave7_root_amount
+			bunny_manager._set_spawn_rate(wave7_bunny_amount)
+			fisherman.start_fish_spawn_timer(wave7_fish_timer)
 			
 			flower_progress = GameState.WAVE_3
 			pass
@@ -101,10 +145,7 @@ func _on_wave_timer_timeout() -> void:
 	# Just wait 5s for now
 	var start_next_wave_timer = Timer.new()
 	add_child(start_next_wave_timer)
-	if(current_state == GameState.WAVE_3):
-		start_next_wave_timer.wait_time = 45.0 # Wait 5.0s for now
-	else:
-		start_next_wave_timer.wait_time = 12.0 # Wait 5.0s for now
+	start_next_wave_timer.wait_time = wave_length[current_state] # Wait 5.0s for now
 	start_next_wave_timer.one_shot = true
 	start_next_wave_timer.connect("timeout", Callable(self, "_on_start_next_wave_timer_timeout"))
 	start_next_wave_timer.start()
@@ -117,7 +158,11 @@ func _on_flower_bed_flowers_died() -> void:
 
 
 func _on_audio_stream_player_finished() -> void:
-	var next_song = main_bg_music[(current_state) - 1]
+	var next_song = null
+	if current_state > main_bg_music.size():
+		next_song = main_bg_music[-1]
+	else:
+		next_song = main_bg_music[(current_state) - 1]
 	if main_audio.stream != next_song:
 		if current_state == GameState.WAVE_3 and not tranistioned_audio:
 			tranistioned_audio = true
