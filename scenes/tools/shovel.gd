58d38b307ed68329e6be39cb517177b2dcfff39e
Markuss Sprogis <markuss.sprogis@dataductus.se>
2025-04-27 12:39:26 +0200

    Add more sounds

--- scenes/tools/shovel.gd
+++ scenes/tools/shovel.gd
@@ -4,10 +4,14 @@ extends Tool
 const TOOL = preload("res://assets/farming_tools/all_tools/Showel.png")
 const TOOL_IN_USE = preload("res://assets/farming_tools/all_tools/ShowelDig.png")
 @onready var sprite_2d: Sprite2D = $Sprite2D
-
+@onready var dig_in_sound = preload("res://assets/audio/shovel_dig.wav")
+#@onready var dig_out_sound = preload("res://assets/audio/shovel_dig_out.wav")
 func start_performing_action():
 	performing_action = true
 	sprite_2d.texture = TOOL_IN_USE
+	$AudioStreamPlayer2D.stream = dig_in_sound
+	$AudioStreamPlayer2D.pitch_scale = randf_range(0.7, 1.0)
+	$AudioStreamPlayer2D.play()
 	pass
 	
 func stop_performing_action():
